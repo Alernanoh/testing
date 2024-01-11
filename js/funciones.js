29c561ade5ee16dddeac1bf89a9f82722c7462a7
Nano <alhernano@gmail.com>
2024-01-11 08:50:11 -0500

    Adding some js test as well

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+//funcion delcarada
+//
+function saludo(){
+  console.log("Hola, buenos días");
+}
+
+saludo();
+
+
+//funcion anonima o flecha
+let saludar = (nombre2) => `Hola, buenos días ${nomvre2}`;
+
+console.log(saludar("Stalin"));
---
+++ js/indes.js
@@ -0,0 +1,14 @@
+//Tabla de multiplicar
+let factor = prompt("Ingrese un número: ");
+
+let num = parseInt(factor);
+
+ for (let i=0; i<=9; i++) {
+      let mult=(i*num); 
+      console.log(`${i}*${num}= ${mult}`);
+  }
+
+
+
+
+
